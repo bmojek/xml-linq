302fa6d21cd7520b066462c219f26a62435d556e
Bartłomiej Mojek <Bartlomiej.Mojek@microsoft.wsei.edu.pl>
2022-06-05 12:44:54 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            string line = "";
+            string s = "";
+            while((line=Console.ReadLine()) != null && line != "")
+            {
+                s += line;
+            }
+
+            var xdoc = XDocument.Parse(s);
+
+            
+            var q = xdoc.Descendants("Customer");
+
+            foreach(var x in q)
+            {
+                x?.Descendants("Order").Select(x => x.Element("CompanyName"));
+
+                Console.WriteLine(x);
+            }
+            
+
+        }
+    }
+}
